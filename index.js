a0146a588470416026a335709c79f96cca8d6ae1
gabrielfreire <gabriel.freire@imsmaxims.com>
2018-06-13 16:28:36 +0100

    resume normal index.js

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const np = new Numpy();
 async function init() {
     let startTime = new Date().getTime();
     try {
-        // let arr = await np.arange(10, 10240, 2);
-        // let max = await np.max(arr);
-        // let sum = await np.sum(arr);
-        // let std = await np.std(arr);
-        // let min = await np.min(arr);
-        // let mean = await np.mean(arr);
-        let train = await np.train(100);
-        // console.log(`max -> ${max}`);
-        // console.log(`min -> ${min}`);
-        // console.log(`sum -> ${sum}`);
-        // console.log(`std -> ${std}`);
-        // console.log(`mean -> ${mean}`);
-        // console.log(`arange array length -> ${arr.length}`);
+        let arr = await np.arange(10, 10240, 2);
+        let max = await np.max(arr);
+        let sum = await np.sum(arr);
+        let std = await np.std(arr);
+        let min = await np.min(arr);
+        let mean = await np.mean(arr);
+        // let train = await np.train(100); // for real-time example
+        console.log(`max -> ${max}`);
+        console.log(`min -> ${min}`);
+        console.log(`sum -> ${sum}`);
+        console.log(`std -> ${std}`);
+        console.log(`mean -> ${mean}`);
+        console.log(`arange array length -> ${arr.length}`);
     } catch(e) {
         console.error(e);
     }
